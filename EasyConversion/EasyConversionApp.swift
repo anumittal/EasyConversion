465ae5e59e28426a604af9b5d8ae1242d2e218e5
Anu Mittal <anu22mittal@gmail.com>
2021-02-07 00:45:13 +0800

    Initial Commit

---
+++ EasyConversion/EasyConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyConversionApp.swift
+//  EasyConversion
+//
+//  Created by Anu Mittal on 07/02/21.
+//
+
+import SwiftUI
+
+@main
+struct EasyConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
